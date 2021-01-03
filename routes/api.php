5eafbe7664661c0fab5671ec5a033c0478716b96
Douglas Ramírez <33143337+Doukuest@users.noreply.github.com>
2021-01-02 21:46:45 -0600

    Routes fixing

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
 
-$limiter = config('fortify.limiters.login');
 
 /*
 |--------------------------------------------------------------------------
@@ -20,15 +18,6 @@ $limiter = config('fortify.limiters.login');
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-
-Route::post('/api/login', [AuthenticatedSessionController::class, 'store'])
-    ->middleware(array_filter([
-        'guest',
-        $limiter ? 'throttle:' . $limiter : null,
-    ]));
-
-Route::post('/api/logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->name('logout');
 
 Route::middleware('auth:sanctum')->group(function () {
     // Your protected routes goes here
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
+
+$limiter = config('fortify.limiters.login');
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{any?}', function () {
     return view('welcome');
 })->where('any','^(?!api\/)[\/\w\.\,-]*');
+
+Route::post('/api/login', [AuthenticatedSessionController::class, 'store'])
+    ->middleware(array_filter([
+        'guest',
+        $limiter ? 'throttle:' . $limiter : null,
+    ]));
+
+Route::post('/api/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->name('logout');
 
